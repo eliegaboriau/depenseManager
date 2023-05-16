24800f28508b2cf4ee69727ca2fe77035b4106bd
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-05-16 14:51:19 +0200

    add seeds

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jquery-rails'
 gem "bootstrap_form", "~> 5.1"
 gem "rails-i18n"
 gem "devise-i18n"
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create users
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password' # Change this to the desired password
+  )
+  puts "User created with email: #{User.last.email} and password: password"
+end
+
+# Create categories for each user
+User.all.each do |user|
+  3.times do
+    Category.create!(
+      name: Faker::Lorem.word,
+      user: user
+    )
+  end
+  puts "Categories created for user #{user.email} with names: #{Category.where(user: user).pluck(:name).join(', ')}"
+end
+
+# Create expenses for each user and associate them with a random category
+User.all.each do |user|
+  10.times do
+    Depense.create!(
+      amount: Faker::Number.between(from: 10, to: 1000),
+      category: Category.where(user: user).sample,
+      date: Faker::Date.between(from: 1.year.ago, to: Date.today),
+      user: user
+    )
+  end
+  puts "Depenses created for user #{user.email}"
+end
